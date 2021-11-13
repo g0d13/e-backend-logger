2bf54ad6f6b288301fb394f549078e51e5420139
Diego <g0d13@outlook.com>
2021-11-13 17:55:54 -0600

    feat: add util for creating jwt token

---
+++ utils/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+
+/// Generate and sign a new jwt token
+function generateJWT(app) {
+  return jwt.sign(
+    app,
+    process.env.SECRET_KEY,
+    { expiresIn: process.env.JWT_EXPIRATION }
+  );
+}
+
+module.exports = generateJWT
