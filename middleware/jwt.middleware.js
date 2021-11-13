c756254170e9931e1b6a312c2789ac1b5c2f0427
Diego <g0d13@outlook.com>
2021-11-13 17:50:37 -0600

    feat: create middleware for check authorization header

---
+++ middleware/jwt.middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = validateToken = (req, res, next) => {
+  // Check header authorization looking for bearer
+  const token = req.header('Authorization')?.split(' ')?.reverse()[0];
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'You must provide a token in headers',
+    });
+  }
+
+  try {
+    req.app = jwt.verify(token, process.env.SECRET_KEY);
+
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Error while decrypting token',
+    });
+  }
+};
