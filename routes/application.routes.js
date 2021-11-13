cca6740a1db70d43505827bee5badcd6b7769259
Diego <g0d13@outlook.com>
2021-11-13 17:57:42 -0600

    feat: add routes for logs

--- routes/application.routes.js
+++ routes/application.routes.js
@@ -2,12 +2,21 @@
 
 const router = require('express').Router();
 
-const prefix = '/application';
+const prefix = '/apps';
 
 const controller = require('../controllers/application.controller');
 
-router.get(`${prefix}/`, controller.findAll);
+const validator = require('express-joi-validation').createValidator({passError: true})
+const { ValidateId } = require('../dto/id')
+
+// find all apps
+router.get(`${prefix}/`, validateToken, controller.findAll);
+
+// generate token for specific app
+router.post(`${prefix}/:id/auth`, validator.params(ValidateId), controller.auth);
+// create a new app
 router.post(`${prefix}/`, controller.create);
+
 
 
 module.exports = router;
