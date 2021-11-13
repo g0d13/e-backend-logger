798f465edb97e01ca17e5194ba210e598575aca0
Diego <g0d13@outlook.com>
2021-11-13 15:20:03 -0600

    feat: add joi express validator middleware

--- app.js
+++ app.js
@@ -18,10 +18,14 @@ const DATABASE_NAME = 'logger'
 mongoose.connect('mongodb://' + HOST_NAME + '/' + DATABASE_NAME);
 
 //Get the default connection
-var db = mongoose.connection;
+const db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
 app.use('/api', require('./routes/logs.routes'));
 app.use('/api', require('./routes/application.routes'))
